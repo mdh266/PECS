3fda6357df11a1f8431e9489534a01fef2b20d26
Mike <mdh266@gmail.com>
2017-02-09 10:07:54 -0500

    adding image to documentation.

--- search/pages_2.js
+++ search/pages_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['readme',['README',['../md___users__mike__documents_cpp_codes__p_e_c_s_2_d__r_e_a_d_m_e.html',1,'']]],
-  ['readme',['README',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['photoelectrochemical_20cell_20simulator',['PhotoElectrochemical Cell Simulator',['../md___users__mike__documents_cpp_codes__p_e_c_s_2_d__r_e_a_d_m_e.html',1,'']]]
 ];
